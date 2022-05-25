3ecba79a54052aa7ff44899e9e4d507880e779b9
dinhdjj <dinhdjj@gmail.com>
2022-05-25 09:35:08 +0700

    chore(linting): uses `eslint` & `prettier`

--- resources/js/Vite/inertia-layout.ts
+++ resources/js/Vite/inertia-layout.ts
@@ -2,13 +2,13 @@ import type { Plugin } from "vite";
 
 const PLUGIN_NAME = "vite:inertia:layout";
 const TEMPLATE_LAYOUT_REGEX =
-    /<template +layout(?: *= *['"]([-_\w\/]+)['"] *)?>/;
+    /<template +layout(?: *= *['"]([-_\w/]+)['"] *)?>/;
 
 /**
  * A basic Vite plugin that adds a <template layout="name"> syntax to Vite SFCs.
  * It must be used before the Vue plugin.
  */
-export default (layouts: string = "@/views/layouts/"): Plugin => ({
+export default (layouts = "@/views/layouts/"): Plugin => ({
     name: PLUGIN_NAME,
     transform: (code: string) => {
         if (!TEMPLATE_LAYOUT_REGEX.test(code)) {
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import laravel from "vite-plugin-laravel";
 import vue from "@vitejs/plugin-vue";
+import eslint from "vite-plugin-eslint";
 import inertia from "./resources/js/Vite/inertia-layout";
 
 export default defineConfig({
     plugins: [
+        eslint(),
         inertia(),
         vue(),
         laravel({
