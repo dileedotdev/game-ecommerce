155be582ab1cb645b9caed3f60652b13ade28204
dinhdjj <dinhdjj@gmail.com>
2022-05-25 13:56:08 +0700

    chore(vue): uses `unplugin-vue-components`

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from "vite";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import laravel from "vite-plugin-laravel";
-import vue from "@vitejs/plugin-vue";
-import eslint from "vite-plugin-eslint";
-import inertia from "./resources/js/Vite/inertia-layout";
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import laravel from 'vite-plugin-laravel';
+import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint';
+import components from 'unplugin-vue-components/vite';
+import inertia from './resources/js/Vite/inertia-layout';
 
 export default defineConfig({
     plugins: [
-        eslint(),
         inertia(),
+        components({
+            dirs: ['resources/js/Components'],
+            dts: 'resources/js/components.d.ts',
+            directoryAsNamespace: true,
+        }),
+        eslint(),
         vue(),
         laravel({
             postcss: [tailwindcss(), autoprefixer()],
