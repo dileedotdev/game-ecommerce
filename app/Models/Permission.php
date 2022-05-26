07d424c62536bc79c0066266e86ae678896d6ab8
dinhdjj <dinhdjj@users.noreply.github.com>
2022-05-26 08:39:57 +0000

    Php-cs-fixer Fixed!

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,6 +4,37 @@ namespace App\Models;
 
 use Spatie\Permission\Models\Permission as Model;
 
+/**
+ * App\Models\Permission.
+ *
+ * @property int                                                         $id
+ * @property string                                                      $name
+ * @property string                                                      $guard_name
+ * @property string|null                                                 $description
+ * @property int                                                         $is_build_in
+ * @property \Illuminate\Support\Carbon|null                             $created_at
+ * @property \Illuminate\Support\Carbon|null                             $updated_at
+ * @property \Illuminate\Database\Eloquent\Collection|Permission[]       $permissions
+ * @property int|null                                                    $permissions_count
+ * @property \Illuminate\Database\Eloquent\Collection|\App\Models\Role[] $roles
+ * @property int|null                                                    $roles_count
+ * @property \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
+ * @property int|null                                                    $users_count
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereGuardName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereIsBuildIn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Permission extends Model
 {
 }
