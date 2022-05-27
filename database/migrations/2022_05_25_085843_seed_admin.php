26a116a279948de506354d501a4dff1d2f6f1eba
dinhdjj <dinhdjj@gmail.com>
2022-05-27 11:05:08 +0700

    chore(permission): convention name permissions

--- database/migrations/2022_05_25_085843_seed_admin.php
+++ database/migrations/2022_05_25_085843_seed_admin.php
@@ -28,15 +28,15 @@ return new class() extends Migration {
 
         $role->givePermissionTo(
             Permission::create(['name' => 'view.admin-page', 'description' => 'Can view admin page (important)']),
-            Permission::create(['name' => 'permissions.view', 'description' => 'Can view any permissions (include important infos)']),
-            Permission::create(['name' => 'roles.view', 'description' => 'Can view any roles (include important infos)']),
-            Permission::create(['name' => 'roles.create', 'description' => 'Can create roles']),
-            Permission::create(['name' => 'roles.update', 'description' => 'Can update any roles']),
-            Permission::create(['name' => 'roles.delete', 'description' => 'Can delete any roles']),
-            Permission::create(['name' => 'users.view', 'description' => 'Can view any users (include important infos)']),
-            Permission::create(['name' => 'users.create', 'description' => 'Can create users']),
-            Permission::create(['name' => 'users.update', 'description' => 'Can update any users']),
-            Permission::create(['name' => 'users.delete', 'description' => 'Can delete any users']),
+            Permission::create(['name' => 'permissions.view.*', 'description' => 'Can view any permissions (include important infos)']),
+            Permission::create(['name' => 'roles.view.*', 'description' => 'Can view any roles (include important infos)']),
+            Permission::create(['name' => 'roles.create.*', 'description' => 'Can create roles']),
+            Permission::create(['name' => 'roles.update.*', 'description' => 'Can update any roles']),
+            Permission::create(['name' => 'roles.delete.*', 'description' => 'Can delete any roles']),
+            Permission::create(['name' => 'users.view.*', 'description' => 'Can view any users (include important infos)']),
+            Permission::create(['name' => 'users.create.*', 'description' => 'Can create users']),
+            Permission::create(['name' => 'users.update.*', 'description' => 'Can update any users']),
+            Permission::create(['name' => 'users.delete.*', 'description' => 'Can delete any users']),
         );
 
         $admin->assignRole($role);
